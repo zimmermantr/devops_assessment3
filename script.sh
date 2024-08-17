0ffbd1ecb209cc8b24ef92d60a0b1b8ad0a634cc
Tristan <tristanzimmerman95@gmail.com>
2024-08-17 12:52:05 -0400

    Add script changes

--- script.sh
+++ script.sh
@@ -7,3 +7,11 @@ docker compose push
 
 # Create and run containers
 docker compose up -d
+
+# Make a file and push it to trigger github actions
+RANDOM_NUMBER=$(date +%s%N) # Using timestamp with nanoseconds for random number
+
+echo "Triggering Github Actions with random number $RANDOM_NUMBER" > trigger.txt
+git add trigger.txt
+git commit -m "all: Trigger Github Actions with random number $RANDOM_NUMBER"
+git push origin master
